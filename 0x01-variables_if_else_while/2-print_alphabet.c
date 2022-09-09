29b893bb3e564faff75772a5add72d24f79a6c54
Zainab Opeloyeru <zainabopeloyeru@gmail.com>
2022-09-09 10:36:34 -0700

    a to z in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print a to z in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (ch = 'a'; ch <= 'z'; ++c)
+	putchar("%c ", c);
+	return (0);
+}
