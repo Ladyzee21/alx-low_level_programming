88533040ffbbbbcf367dde1a3e016e50a4fcb9e3
Zainab Opeloyeru <zainabopeloyeru@gmail.com>
2022-09-13 04:58:05 -0700

    my putchar program

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - entry point for this program
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char a = z;
+
+	_putchar('m');
+	_putchar('a');
+	_putchar('i');
+	_putchar('n');
+	_putchar('\n');
+
+return (0);
+}
