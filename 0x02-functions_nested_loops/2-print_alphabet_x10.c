e0d4a9a84f75835a8880db6d3bcb1a3e76d01b72
Zainab Opeloyeru <zainabopeloyeru@gmail.com>
2022-09-13 05:49:26 -0700

    prints alphabets ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - entry point for printing alphabetsten times
+ *
+ * Return:Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int b = 0;
+
+	while (b < 10)
+		{
+		char a = 'a';
+
+		while (a <= 'z')
+		{
+		_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+			b++;
+		}
+}
