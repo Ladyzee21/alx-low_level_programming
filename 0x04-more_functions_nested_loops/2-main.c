08facf1529199fdc767d0f396c9c3bc99185ef0a
Zainab Opeloyeru <zainabopeloyeru@gmail.com>
2022-09-15 03:21:08 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,22 @@
+nclude "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	a = 98;
+	b = 1024
+	{
+	printf("%d\n", a * b);
+	}
+	a = '-402';
+	b = 4096;
+	{
+	printf("%d\n", a * b);
+	}
+	return (0);
+}
