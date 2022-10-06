a4dfeba671f156ad94a9b1132b084cf6c73c2591
Zainab Opeloyeru <zainabopeloyeru@gmail.com>
2022-10-06 09:17:06 -0700

    a main header file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,5 +8,6 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 
 char *_memset(char *new_ptr, char c, unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif /* MAIN_H*/
