c65af897eca3c8d6beb7f049f15723f9098d9ee9
zainabopeloyeru@gmail.com <zainabopeloyeru@gmail.com>
2022-10-27 13:25:40 -0700

    correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,29 +6,28 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-		unsigned int uint, place;
-		int n;
+		unsigned int ui;
+		int len, base_two;
 
 	if (!b)
-		return (0);
+	return (0);
 
-		for (n = 0; b[n]; n++)
-			;
-			n--;
-	for (place = 1, uint = 0; n >= 0; n--)
+			ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-	if (b[n] == '0')
+	if (b[len] != '0' && b[len] != '1')
 	{
-	place *= 2;
-			continue;
-	}
-	else if (b[n] == '1')
-										{
-	uint += place;
-	place *= 2;
-		continue;
-		}
 		return (0);
 	}
-		return (uint);
+
+	if (b[len] & 1)
+		{
+		ui += base_two;
+		}
+	}
+		return (ui);
 }
